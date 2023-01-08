f217fda358f6d92c300e8b65db68a4d9b189b6cb
Manecoon <stas_on.85@mail.ru>
2023-01-08 14:36:42 +0300

    task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -3,5 +3,12 @@
 Console.Write("Введите цифру: ");
 int num = Convert.ToInt32(Console.ReadLine());
 if (num < 100) Console.Write("Третьей цифры нет");
-while (num > 1000) num = num / 10;
-Console.Write($"Третья цифра заданного числа: {num % 10}");
+if (num >= 100 && num < 1000) Console.Write($"Третья цифра заданного числа: {num % 10}");
+if (num >= 1000)
+{
+    {
+        while (num > 1000)
+            num = num / 10;
+    }
+    Console.Write($"Третья цифра заданного числа: {num % 10}");
+}
